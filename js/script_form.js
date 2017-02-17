2a2aa1e9825929048e6c1e3047b3b6716dd03f86
yrijoleynik <yurij@ua.fm>
2017-02-17 14:46:57 +0200

    добавлено событие на TAB

--- js/script_form.js
+++ js/script_form.js
@@ -7,6 +7,15 @@ $( function () {
 
     });
 
+    $('.text').focusin( function () {
+        $(this).next().addClass('active-form');
+    });
+
+        $('.text').focusout( function () {
+            $(this).next().removeClass('active-form');
+
+    });
+
     $('.button').on('click', function (e) {
         e.preventDefault();
         $('.hint').addClass('active-form');
